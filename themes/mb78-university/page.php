65ba2bee3e1b39ce0505a0d376dcb2937e6c8f76
Mauro Bono <bono.mauro@gmail.com>
2018-08-02 08:51:44 +0000

    add Wordpress Loop and create single.php and page.php

--- themes/mb78-university/page.php
+++ themes/mb78-university/page.php
@@ -1,9 +1,9 @@
 <h1><?php bloginfo('title'); ?></h1>
-
+<h1>This is a page not a post</h1>
 <!--WORDPRESS LOOP-->
 <?php if( have_posts() ) : while ( have_posts() ) : ?>
 <?php the_post(); ?>
-  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  <h2><?php the_title(); ?></h2>
   <p><?php the_content(); ?></p>
   <hr>
 <?php endwhile; ?>
--- themes/mb78-university/single.php
+++ themes/mb78-university/single.php
@@ -3,7 +3,7 @@
 <!--WORDPRESS LOOP-->
 <?php if( have_posts() ) : while ( have_posts() ) : ?>
 <?php the_post(); ?>
-  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  <h2><?php the_title(); ?></h2>
   <p><?php the_content(); ?></p>
   <hr>
 <?php endwhile; ?>
