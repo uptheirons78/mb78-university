1b38b41ae0804846956029f39caf945503a9b5fe
Mauro Bono <bono.mauro@gmail.com>
2018-08-30 14:05:23 +0000

    Add live search

--- themes/mb78-university/footer.php
+++ themes/mb78-university/footer.php
@@ -69,10 +69,24 @@
     </div>
     
    </footer>
-	
+   
+	 <div id="ricerca" class="search-overlay">
+	   
+	   <div class="search-overlay__top">
+	     <div class="container">
+	       <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+	       <input type="text" name="" class="search-term" placeholder="what are you looking for ?" id="search-term" />
+	       <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+	     </div>
+	   </div>
+	   
+	   <div class="container">
+	     <div class="search-overlay__results">
+	       
+	     </div>
+	   </div>
+	 </div>
+	 
 	<?php wp_footer(); ?>
-	<script id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:8082/browser-sync/browser-sync-client.js?v=2.24.4'><\/script>".replace("HOST", location.hostname));
-//]]></script>
 </body>
 </html>
